c7fba590fc1899a024342ab040ddcfda696e6c16
PoreEdna <edna.pore@evsu.edu.ph>
2021-04-06 15:55:54 +0800

    commit files

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +10,49 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  contName="";
+  contNumber="";
+  contacts=[];
+  constructor(public alertController: AlertController)  {}
+
+  saveC() {
+    let contact = {
+      name: this.contName,
+      number: this.contNumber
+
+    }
+    this.contacts.push(contact);
+    console.log(this.contacts);
+    this.clearField();
+
+  }
+  clearField() {
+    this.contName = "";
+    this.contNumber = "";
+  }
+  
+
+  async delete(i) {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Delete this Contact?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+          }
+        }, {
+          text: 'Confirm',
+          handler: () => {
+            this.contacts.splice(i,1);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
 
 }
